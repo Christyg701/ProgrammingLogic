02c8d1e169f211d493951699092f0c6b858a75b6
Christyg701 <christyg701@yahoo.com>
2025-03-24 21:32:03 +0000

    Created Lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    //Part 1 Variable Declarations//   
+    int myNum=20;
+    Console.WriteLine (myNum);
+    double myDoubleNum=8.999D;
+    Console.WriteLine (myDoubleNum);
+    float myFraction=1.2345F;
+    Console.WriteLine (myFraction);
+    char myFavLetter="C";
+    console.WriteLine (myFavLetter);
+    bool myBool=true;
+    console.WriteLine (myBool)
+    string name="John Smith";
+    Console. WriteLine(name);
+    }
+}
