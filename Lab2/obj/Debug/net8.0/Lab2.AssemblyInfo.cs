6705fd446aabf3d2cce666cefdd67dd1f7768e8d
Christyg701 <christyg701@yahoo.com>
2025-04-18 22:24:09 +0000

    Lab5 Completed

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f7fb904a3310b28acfedc6ca04a8f0cc1f1f7be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df61f826f10a917a10acac29fb0e34283d2fcee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab5/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace Lab5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+Console.ForegroundColor= ConsoleColor.DarkMagenta;   
+Console.WriteLine("Problem 1: Seasons of the Year:");
+Console.ResetColor();
+String []seasons= new string[4] {"Spring","Summer","Fall", "Winter"};
+for (int i=0; i <= 3; i++)
+{
+Console.WriteLine("Season: "+(seasons[i]));
+}
+
+
+
+Console.ForegroundColor= ConsoleColor.DarkMagenta;   
+Console.WriteLine("Problem 2: Days of the Week:");
+Console.ResetColor();
+
+String[]daysofweek={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+Console.Write("Enter day of the week (1-7)");
+int day=Convert.ToInt32(Console.ReadLine());
+if (day>=1&&day<=7)
+{
+Console.WriteLine("That day is: "+ daysofweek[day-1]);
+}
+else
+{
+ Console.WriteLine("Invalid Input, please enter a number from 1-7");
+}
+{
+    
+    
+    {
+
+Console.ForegroundColor= ConsoleColor.DarkMagenta;   
+Console.WriteLine("Problem 3: Favorite Books and Authors:");
+Console.ResetColor();
+
+string[] books={"Grapes of Wrath","Where the Crawdads Sing","The Motivation Code" };
+string[] authors={"John Steinbeck","Delia Owens","Todd Henry"};
+for (int b=0; b<3;b++)
+{ Console.WriteLine((books[b])+" by "+(authors[b]));
+}}}
+Console.ForegroundColor= ConsoleColor.DarkMagenta;   
+Console.WriteLine("Problem 4: Temperature Tracker:");
+Console.ResetColor();  
+
+int[] temperatures= new int[5] {65,72,78,70,68};
+Array.Sort(temperatures);
+Console.WriteLine("Sorted Temperatures: ");
+foreach (int temp in temperatures)
+{
+Console.WriteLine(temp+" ");
+}
+{
+Console.WriteLine("Highest Temperature: " + (temperatures.Max()));
+}
+{
+Console.WriteLine("Lowest Temperature: " + (temperatures.Min()));
+}
+{Console.ForegroundColor= ConsoleColor.DarkMagenta;   
+Console.WriteLine("Problem 5: Reverse Countdown:");
+Console.ResetColor();  
+int[] countdown={5,4,3,2,1};
+Array.Reverse(countdown);
+Console.WriteLine("Countdown: ");
+foreach(int num in countdown)
+{
+Console.WriteLine(num+" ");
+}
+
+}
+}
+}
+  
