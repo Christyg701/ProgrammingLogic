ffa6155ad6999c045b9d6dcff3ac2fe4cb54fe37
Christyg701 <christyg701@yahoo.com>
2025-04-25 18:30:00 +0000

    Lab6 Completed

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df61f826f10a917a10acac29fb0e34283d2fcee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6705fd446aabf3d2cce666cefdd67dd1f7768e8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
